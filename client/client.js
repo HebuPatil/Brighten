4480441802bf32ebc022c47e094524c817e9e017
Hebu Patil <hebupatil@gmail.com>
2020-06-06 19:20:45 -0700

    fully functional
    works with bad word filter

--- client/client.js
+++ client/client.js
@@ -2,9 +2,12 @@ console.log('Hello World!')
 
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const messageElement = document.querySelector('.messages')
 const API_URL = 'http://localhost:5000/messages'
 
 loadingElement.style.display = 'none';
+
+listAllMessages();
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -29,6 +32,37 @@ form.addEventListener('submit', (event) => {
         }
     }).then(response => response.json())
       .then(createdMessage => {
-        console.log(createdMessage);
+        form.reset();
+        form.style.display = '';
+        listAllMessages();
       });
 });
+
+function listAllMessages() {
+    messageElement.innerHTML = '';
+    fetch(API_URL) 
+        .then(response => response.json())
+        .then(messages => {
+            console.log(messages);
+            messages.reverse();
+            messages.forEach(message => {
+                const div = document.createElement('div');
+
+                const header= document.createElement('h3');
+                header.textContent = message.name;
+
+                const contents = document.createElement('p');
+                contents.textContent = message.content;
+
+                const date = document.createElement('small');
+                date.textContent = new Date(message.created);
+
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(date);
+
+                messageElement.appendChild(div);
+            });
+            loadingElement.style.display = 'none';
+        });
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const monk = require('monk');
+const Filter = require('bad-words');
 
 const app  = express();
 const db = monk('localhost/Brighten');
 const allMessages = db.get('messages');
+const filter = new Filter({ placeHolder: '❤️'});
 
 app.use(cors()); //cors issue
 app.use(express.json()); //json parser
@@ -17,15 +19,23 @@ app.get('/', (req, res) => {
 
   /// INSTEAD OF THSI USE YUP OR JOI (VALIDATION)
 function isValidMessage(userMessage) {
-    return userMessage.name && userMessage.name.toString().trim() !== '' && userMessage.message && userMessage.message.toString().trim()
+    return userMessage.name && userMessage.name.toString().trim() !== '' && userMessage.name.toString().trim().length <= 50 &&
+    userMessage.message && userMessage.message.toString().trim() !== '' && userMessage.message.toString().trim().length <= 140;
 }
 
+app.get('/messages', (req, res) => {
+    allMessages
+        .find()
+        .then(mews => {
+            res.json(mews);
+        })
+})
 
 app.post('/messages', (req, res) => {
     if(isValidMessage(req.body)) {
         const userMessage = {
-            name: req.body.name.toString(),
-            content: req.body.message.toString(),
+            name: filter.clean(req.body.name.toString()),
+            content: filter.clean(req.body.message.toString()),
             created: new Date()
         };
         allMessages
