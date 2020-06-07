0f83495318c37a31d812d79deec23a244d9b1e57
Hebu Patil <hebupatil@gmail.com>
2020-06-06 17:12:34 -0700

    update

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const monk = require('monk');
+
+const app  = express();
+const db = monk('localhost/Brighten');
+const allMessages = db.get('messages');
+
+app.use(cors()); //cors issue
+app.use(express.json()); //json parser
+
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello, Brighten'
+    });
+  });
+
+  /// INSTEAD OF THSI USE YUP OR JOI (VALIDATION)
+function isValidMessage(userMessage) {
+    return userMessage.name && userMessage.name.toString().trim() !== '' && userMessage.message && userMessage.message.toString().trim()
+}
+
+
+app.post('/messages', (req, res) => {
+    if(isValidMessage(req.body)) {
+        const userMessage = {
+            name: req.body.name.toString(),
+            content: req.body.message.toString(),
+            created: new Date()
+        };
+        allMessages
+            .insert()
+            .then(createdMessage => {
+                res.json(createdMessage);
+            })
+    } else {
+        res.status(422);
+        res.json({
+            message: 'Please send a thoughtful message...'
+        });
+    }
+});
+
+app.listen(5000, () => {
+    console.log('Listening on http://localhost:5000')
+})
